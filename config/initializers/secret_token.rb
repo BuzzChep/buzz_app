b8eb36272aeaef36325ae72846046556aba36da2
Buzz Chep <busechp@hotmail.com>
2011-08-18 14:39:28 +0100

    Initial commit by Buzz Chep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuzzApp::Application.config.secret_token = 'a285eba518c546c6878746e22c530bdd6eec722d5ce5742a78e9c699ef5618fbc640c2ecf522eff90f5d208dce2a34c23b3c26281f76fe08d19d6d924aad870f'
